ac5e395d26b5e87f73a125cac508e80544ff8b87
Xiang Zhi <xiangzhi@Jules-Ubuntu.(none)>
2014-04-05 21:59:13 -0500

    fixed

--- P3/Linux/test.c
+++ P3/Linux/test.c
@@ -1,20 +1,45 @@
-#include "mem.h"
-#include <string.h>
-#include <stdint.h>
-#include <stdio.h>
+/* use smallest free space for allocation */
 #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "mem.h"
 
-int main(int argc, char *argv[]){
-    Mem_Init(2048);
-    void* ptr[9];
-    ptr[0] = Mem_Alloc(100);
-    ptr[1] = Mem_Alloc(20);
-    ptr[2] = Mem_Alloc(10);
-    ptr[3] = Mem_Alloc(100);
-    Mem_Dump();
-    Mem_Free(ptr[0]);
-    Mem_Free(ptr[2]);
-    ptr[2] = Mem_Alloc(3);
-    Mem_Dump();
-	return 0;
+int main() {
+   assert(Mem_Init(4096) == 0);
+   void * ptr[6];
+   ptr[0] = Mem_Alloc(400);
+   assert(ptr[0] != NULL);
+
+   ptr[1] = Mem_Alloc(400);
+   assert(ptr[1] != NULL);
+
+   ptr[2] = Mem_Alloc(1000);
+   assert(ptr[2] != NULL);
+
+   ptr[3] = Mem_Alloc(1000);
+   assert(ptr[3] != NULL);
+
+   ptr[4] = Mem_Alloc(400);
+   assert(ptr[4] != NULL);
+
+   ptr[5] = Mem_Alloc(400);
+   assert(ptr[5] != NULL);
+   while (Mem_Alloc(360) != NULL);
+   assert(Mem_Free(ptr[0]) == 0);
+   ptr[0] = NULL;
+
+   assert(Mem_Free(ptr[2]) == 0);
+   ptr[2] = NULL;
+   assert(Mem_Free(ptr[4]) == 0);
+   ptr[4] = NULL;
+
+   ptr[0] = Mem_Alloc(360);
+   assert(ptr[0] != NULL);
+   ptr[2] = Mem_Alloc(360);
+   assert(ptr[2] != NULL);
+
+   ptr[4] = Mem_Alloc(960);
+   assert(ptr[4] != NULL);
+
+   exit(0);
 }
