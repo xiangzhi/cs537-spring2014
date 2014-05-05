f1471e222f0c173f5006ccc1355778d70fefa95d
Xiang Zhi Tan <tan.xiang.zhi@gmail.com>
2014-05-05 03:02:09 -0500

    added restart for 2 sends

--- P5/mfs.c
+++ P5/mfs.c
@@ -5,7 +5,7 @@
 int send_fd;
 struct sockaddr_in saddr;
 
-int udp_Send(char* sendMsg, char* reply);
+int udp_Send(char* sendMsg, char* reply, bool wait);
 
 int MFS_Init(char *hostname, int port){
 
@@ -20,7 +20,7 @@ int MFS_Init(char *hostname, int port){
     char buffer[BUFFER_SIZE];
 
     sprintf(message, "I");
-    udp_Send(message, buffer);
+    udp_Send(message, buffer, true);
     return 0;
 }
 
@@ -34,7 +34,7 @@ int MFS_Lookup(int pinum, char *name){
     char message[BUFFER_SIZE];
     char reply[BUFFER_SIZE];
     snprintf(message, BUFFER_SIZE, "L:%d:%s", pinum, name);
-    udp_Send(message, reply);
+    udp_Send(message, reply, true);
     return atoi(reply);
 }
 
@@ -48,7 +48,7 @@ int MFS_Stat(int inum, MFS_Stat_t *m){
     char message[BUFFER_SIZE];
     char reply[BUFFER_SIZE];
     snprintf(message, BUFFER_SIZE, "S:%d", inum);
-    udp_Send(message, reply);
+    udp_Send(message, reply, true);
     MFS_Stat_t r_stat = *((MFS_Stat_t *)reply);
     //state is incoorect
     if(r_stat.type > 1 || r_stat.type < 0){
@@ -65,17 +65,21 @@ int MFS_Write(int inum, char *buffer, int block){
     if(inum < 0 || block < 0 || block >= 14){
         return -1;
     }
-
+    int status = -1;
     char message[BUFFER_SIZE];
     char reply[BUFFER_SIZE];
     snprintf(message, BUFFER_SIZE, "W:%d:%d", inum, block);
-    udp_Send(message, reply);
-    //make sure the reply is 0;//success
-    if(atoi(reply) != 0){
-        return -1;
+    udp_Send(message, reply, true);
+
+    while(status == -1){
+        udp_Send(message, reply, true);
+        //make sure the reply is 0;//success
+        if(atoi(reply) != 0){
+            return -1;
+        }
+        status = udp_Send(buffer, reply, false);
     }
 
-    udp_Send(buffer, reply);
     //make sure the reply is 0;//success
     if(atoi(reply) != 0){
         return -1;
@@ -89,17 +93,20 @@ int MFS_Read(int inum, char *buffer, int block){
     if(inum < 0 || block < 0 || block >= 14){
         return -1;
     }
-
+    int status = -1;
     char message[BUFFER_SIZE];
     char reply[BUFFER_SIZE];
     snprintf(message, BUFFER_SIZE, "R:%d:%d:p1", inum, block);
-    udp_Send(message, reply);
-    //make sure the reply is 0;//success
-    if(atoi(reply) != 0){
-        return -1;
+    while(status == -1){
+        udp_Send(message, reply, true);
+        //make sure the reply is 0;//success
+        if(atoi(reply) != 0){
+            return -1;
+        }
+        snprintf(message, BUFFER_SIZE, "R:%d:%d:p2", inum, block);
+        status = udp_Send(message, buffer, false);    
     }
-    snprintf(message, BUFFER_SIZE, "R:%d:%d:p2", inum, block);
-    udp_Send(message, buffer);
+
     //write(STDOUT_FILENO,buffer,strlen(buffer) + 1);
     //write(STDOUT_FILENO,"\n",1);
 
@@ -117,7 +124,7 @@ int MFS_Creat(int pinum, int type, char *name){
     char message[BUFFER_SIZE];
     char reply[BUFFER_SIZE];
     snprintf(message, BUFFER_SIZE, "C:%d:%d:%s", pinum, type, name);
-    udp_Send(message, reply);
+    udp_Send(message, reply, true);
     return atoi(reply);
 }
 
@@ -131,18 +138,19 @@ int MFS_Unlink(int pinum, char *name){
     char message[BUFFER_SIZE];
     char reply[BUFFER_SIZE];
     snprintf(message, BUFFER_SIZE, "U:%d:%s", pinum, name);
-    udp_Send(message, reply);
+    udp_Send(message, reply, true);
     return atoi(reply);
 }
 
 int MFS_Shutdown(){
     char message[BUFFER_SIZE];
+    char reply[BUFFER_SIZE];
     snprintf(message, BUFFER_SIZE, "E:");
-    UDP_Write(send_fd, &saddr, message, BUFFER_SIZE);
+    udp_Send(message, reply, false);
     return 0;
 }
 
-int udp_Send(char* sendMsg, char* reply){
+int udp_Send(char* sendMsg, char* reply, bool wait){
 
     while(true){
         //initalize the varaibles
@@ -177,6 +185,11 @@ int udp_Send(char* sendMsg, char* reply){
             }
             //printf("CLIENT:: read %d bytes (message: '%s')\n", status, reply);
             break;
+        }
+        else{
+            if(!wait){
+                return -1;
+            }
         }
     }
     return 0;
